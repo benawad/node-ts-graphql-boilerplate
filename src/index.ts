7bcc5a20028c2715fecf92e8609afae2ec0284f7
ben awad <benawad97@gmail.com>
2018-03-02 11:09:47 -0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { GraphQLServer } from "graphql-yoga";
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: (_: any, { name }: any) => `hhello ${name || "World"}`
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log("Server is running on localhost:4000"));
