cb3a83dc31ee71bab676bac46b4601cb4fff1c86
ben awad <benawad97@gmail.com>
2018-03-03 12:24:16 -0600

    bad config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import "reflect-metadata";
 import { GraphQLServer } from "graphql-yoga";
+import { createConnection } from "typeorm";
 
 const typeDefs = `
   type Query {
@@ -13,4 +15,6 @@ const resolvers = {
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
-server.start(() => console.log("Server is running on localhost:4000"));
+createConnection().then(() => {
+  server.start(() => console.log("Server is running on localhost:4000"));
+});
